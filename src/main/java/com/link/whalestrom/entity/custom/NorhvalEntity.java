ec267bb5014a4f51e69a411fe61e8b24701383ef
Link4real <simon@die-duerrs.de>
2023-12-23 19:55:13 +0100

    Smaller Changes

--- src/main/java/com/link/whalestrom/entity/custom/NorhvalEntity.java
+++ src/main/java/com/link/whalestrom/entity/custom/NorhvalEntity.java
@@ -2,9 +2,11 @@ package com.link.whalestrom.entity.custom;
 
 import com.link.whalestrom.Whalestrom;
 import com.link.whalestrom.entity.ModEntities;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.AnimationState;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.control.MoveControl;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
@@ -12,19 +14,25 @@ import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.FlyingEntity;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.EnumSet;
@@ -33,6 +41,7 @@ public class NorhvalEntity extends FlyingEntity {
     public static final Ingredient BREEDING_INGREDIENT = Ingredient.ofItems(Items.COD);
     public NorhvalEntity(EntityType<? extends FlyingEntity> entityType, World world) {
         super(entityType, world);
+        this.setStepHeight(1.0f);
         this.experiencePoints = 30;
         this.moveControl = new NorhvalMoveControl(this);
     }
@@ -48,10 +57,18 @@ public class NorhvalEntity extends FlyingEntity {
     public static DefaultAttributeContainer.Builder createNorhvalAttributes() {
      return MobEntity.createMobAttributes()
              .add(EntityAttributes.GENERIC_MAX_HEALTH, 30)
-             .add(EntityAttributes.GENERIC_FLYING_SPEED, 0.2f)
+             .add(EntityAttributes.GENERIC_FLYING_SPEED, 0.1f)
              .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 0)
-             .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2f);
+             //.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.1f)
+        ;
 
+    }
+
+    public static boolean canSpawn(EntityType<NorhvalEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+        BlockState blockState = world.getBlockState(pos);
+        return random.nextInt(6) == 0 && pos.getY() > 40 && pos.getY() - world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos).getY() > 20 && blockState.isAir()
+                && world.getEntitiesByClass(PhantomEntity.class, new Box(pos).expand(80D), EntityPredicates.EXCEPT_SPECTATOR).isEmpty()
+                && SpawnHelper.isClearForSpawn(world, pos, blockState, blockState.getFluidState(), ModEntities.NORHVAL);
     }
     public boolean isBreedingItem(ItemStack stack) {
         return BREEDING_INGREDIENT.test(stack);
@@ -83,74 +100,99 @@ public class NorhvalEntity extends FlyingEntity {
         }
     }
 
+
     // Norhval flies randomly through the air:
     static class FlyRandomlyGoal  extends Goal {
         private final NorhvalEntity norhval;
-        public FlyRandomlyGoal(NorhvalEntity norhval) {
-            this.norhval = norhval;
-            this.setControls(EnumSet.of(Goal.Control.MOVE));
-        }
-        @Override
-        public boolean canStart() {
-            double f;
-            double e;
-            MoveControl moveControl = this.norhval.getMoveControl();
-            if (!moveControl.isMoving()) {
-                return true;
+            public FlyRandomlyGoal(NorhvalEntity norhval) {
+                this.norhval = norhval;
+                this.setControls(EnumSet.of(Goal.Control.MOVE));
             }
-            double d = moveControl.getTargetX() - this.norhval.getX();
-            double g = d * d + (e = moveControl.getTargetY() - this.norhval.getY()) * e + (f = moveControl.getTargetZ() - this.norhval.getZ()) * f;
-            return g < 1.0 || g > 3600.0;
-        }
-        @Override
-        public boolean shouldContinue() {
-            return false;
-        }
+
             @Override
-        public void start() {
-            Random random = this.norhval.getRandom();
-            double d = this.norhval.getX() + (double)((random.nextFloat() * 2.0f - 1.0f) * 16.0f);
-            double e = this.norhval.getY() + (double)((random.nextFloat() * 2.0f - 1.0f) * 16.0f);
-            double f = this.norhval.getZ() + (double)((random.nextFloat() * 2.0f - 1.0f) * 16.0f);
-            this.norhval.getMoveControl().moveTo(d, e, f, 1.0);
+            public boolean canStart() {
+                MoveControl moveControl = this.norhval.getMoveControl();
+                if (!moveControl.isMoving()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            @Override
+            public boolean shouldContinue() {
+                return false;
+            }
+
+            @Override
+            public void start() {
+                Random random = this.norhval.getRandom();
+                double randomX = 48D + (random.nextDouble() * 2.0D - 1.0D) * 128.0D;
+                double randomZ = 48D + (random.nextDouble() * 2.0D - 1.0D) * 128.0D;
+
+                double d = this.norhval.getX() + randomX;
+                double e = this.norhval.getY() + (double) ((random.nextDouble() * 2.0F - 1.0F) * 16.0F);
+                double f = this.norhval.getZ() + randomZ;
+
+                if (this.norhval.getY() < 40D)
+                    e = this.norhval.getY() + 16D * random.nextDouble();
+                else if (this.norhval.getY() < 65D)
+                    e = this.norhval.getY() + 40D * random.nextDouble();
+                else if (this.norhval.getY() > 300D)
+                    e = this.norhval.getY() - 26D * random.nextDouble();
+
+                this.norhval.getMoveControl().moveTo(d, e, f, 1.0D);
+            }
         }
+
+
+    @Override
+    public boolean canBeLeashedBy(PlayerEntity player) {
+        return true;
     }
-
-
-    static class NorhvalMoveControl extends MoveControl {
+    private static class NorhvalMoveControl extends MoveControl {
         private final NorhvalEntity norhval;
         private int collisionCheckCooldown;
+        private int waitCooldown;
+
         public NorhvalMoveControl(NorhvalEntity norhval) {
             super(norhval);
             this.norhval = norhval;
         }
+
         @Override
         public void tick() {
-            if (this.state != MoveControl.State.MOVE_TO) {
-                return;
-            }
-            if (this.collisionCheckCooldown-- <= 0) {
-                this.collisionCheckCooldown += this.norhval.getRandom().nextInt(5) + 2;
-                Vec3d vec3d = new Vec3d(this.targetX - this.norhval.getX(), this.targetY - this.norhval.getY(), this.targetZ - this.norhval.getZ());
-                double d = vec3d.length();
-                if (this.willCollide(vec3d = vec3d.normalize(), MathHelper.ceil(d))) {
-                    this.norhval.setVelocity(this.norhval.getVelocity().add(vec3d.multiply(0.1)));
-                } else {
-                    this.state = MoveControl.State.WAIT;
+            if (this.state == MoveControl.State.MOVE_TO) {
+                if (this.collisionCheckCooldown-- <= 0) {
+                    this.collisionCheckCooldown += 3;
+                    Vec3d vec3d = new Vec3d(this.targetX - this.norhval.getX(), this.targetY - this.norhval.getY(), this.targetZ - this.norhval.getZ());
+                    double d = Math.sqrt(vec3d.x * vec3d.x + vec3d.y * vec3d.y + vec3d.z * vec3d.z);
+                    vec3d = new Vec3d(vec3d.x / d, vec3d.y / d, vec3d.z / d);
+                    if ((!this.willCollide() && d > 1) || (this.waitCooldown++ < 0 && d > 1)) {
+                        this.norhval.setVelocity(vec3d.multiply(0.12));
+                    } else {
+                        this.state = MoveControl.State.WAIT;
+                        if (this.waitCooldown++ >= 200) {
+                            this.waitCooldown = -40;
+                        }
+                    }
                 }
             }
         }
-        private boolean willCollide(Vec3d direction, int steps) {
+
+        private boolean willCollide() {
             Box box = this.norhval.getBoundingBox();
-            for (int i = 1; i < steps; ++i) {
-                box = box.offset(direction);
-                if (this.norhval.getWorld().isSpaceEmpty(this.norhval, box)) continue;
+            box = box.expand(0.2D);
+            if (this.norhval.getWorld().isSpaceEmpty(this.norhval, box)) {
                 return false;
             }
             return true;
         }
     }
-
+    @Override
+    public boolean isPushable() {
+        return true;
+    }
     //Norhval looks at direction, it is flying towards
     private static class LookAtDirectionGoal extends Goal {
         private final NorhvalEntity norhval;
